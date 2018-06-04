74269b9fe3353c72de5ce10d32733011f97f0b25
Vishnu <vishnu.kottu95@gmail.com>
2018-06-05 00:15:53 +0530

    First commit

---
+++ cawsworkapp/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace cawsworkapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.task1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.task2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // task1
+            // 
+            this.task1.Location = new System.Drawing.Point(174, 88);
+            this.task1.Name = "task1";
+            this.task1.Size = new System.Drawing.Size(91, 40);
+            this.task1.TabIndex = 0;
+            this.task1.Text = "Task1";
+            this.task1.UseVisualStyleBackColor = true;
+            this.task1.Click += new System.EventHandler(this.task1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(115, 197);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 7;
+            // 
+            // task2
+            // 
+            this.task2.Location = new System.Drawing.Point(539, 88);
+            this.task2.Name = "task2";
+            this.task2.Size = new System.Drawing.Size(91, 40);
+            this.task2.TabIndex = 8;
+            this.task2.Text = "Task2";
+            this.task2.UseVisualStyleBackColor = true;
+            this.task2.Click += new System.EventHandler(this.task2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.task2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.task1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button task1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button task2;
+    }
+}
+
---
+++ cawsworkapp/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace cawsworkapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void task1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                string[] stringArray;
+
+                string[] stringArray1;
+
+                label4.Text = "";
+
+
+                var ds = new DataSet("CSV File");
+                var filename = @"D:\work\Data.csv";
+                var connString = string.Format(
+                    @"Provider=Microsoft.Jet.OleDb.4.0; Data Source={0};Extended Properties=""Text;HDR=YES;FMT=Delimited""",
+                    Path.GetDirectoryName(filename)
+                );
+                using (var conn = new OleDbConnection(connString))
+                {
+                    conn.Open();
+                    var query = "SELECT * FROM [" + Path.GetFileName(filename) + "]";
+                    using (var adapter = new OleDbDataAdapter(query, conn))
+                    {
+
+                        adapter.Fill(ds);
+                    }
+                }
+
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    label4.Text = label4.Text + ds.Tables[0].Rows[i]["Full Name (Billing)"].ToString().Replace(" ", ",") + "," + ds.Tables[0].Rows[i]["Address 1"].ToString().Replace("|", ",") + "\n";
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+
+
+        }
+
+        private void task2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label4.Text = "";
+
+
+                Dictionary<string, string> dict = new Dictionary<string, string>();
+
+
+                var ds1 = new DataSet("CSV File");
+                var filename = @"D:\work\Data.csv";
+                var connString = string.Format(
+                    @"Provider=Microsoft.Jet.OleDb.4.0; Data Source={0};Extended Properties=""Text;HDR=YES;FMT=Delimited""",
+                    Path.GetDirectoryName(filename)
+                );
+                using (var conn = new OleDbConnection(connString))
+                {
+                    conn.Open();
+                    var query = "SELECT * FROM [" + Path.GetFileName(filename) + "]";
+                    using (var adapter = new OleDbDataAdapter(query, conn))
+                    {
+
+                        adapter.Fill(ds1);
+
+                    }
+                }
+
+
+                for (int i = 0; i < ds1.Tables[0].Rows.Count; i++)
+                {
+
+                    dict.Add(ds1.Tables[0].Rows[i][ds1.Tables[0].Columns[0].ColumnName].ToString(), ds1.Tables[0].Rows[i]["Full Name (Billing)"].ToString());
+
+                }
+
+
+
+
+
+
+
+
+                var ds2 = new DataSet("CSV File");
+                var filename2 = @"D:\work\Data_2.csv";
+                var connString2 = string.Format(
+                    @"Provider=Microsoft.Jet.OleDb.4.0; Data Source={0};Extended Properties=""Text;HDR=YES;FMT=Delimited""",
+                    Path.GetDirectoryName(filename2)
+                );
+
+                using (var conn = new OleDbConnection(connString2))
+                {
+                    conn.Open();
+                    var query = "SELECT * FROM [" + Path.GetFileName(filename2) + "]";
+                    using (var adapter = new OleDbDataAdapter(query, conn))
+                    {
+
+                        adapter.Fill(ds2);
+                    }
+                }
+
+                for (int i = 0; i < ds2.Tables[0].Rows.Count; i++)
+                {
+                    label4.Text = label4.Text + ds2.Tables[0].Rows[i][ds2.Tables[0].Columns[0].ColumnName].ToString()+ "," + ds2.Tables[0].Rows[i]["Id"].ToString()+"," + ds2.Tables[0].Rows[i]["Invoice Amount"].ToString() +","+dict[ds2.Tables[0].Rows[i]["Id"].ToString()] + "\n";
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
